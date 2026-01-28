94ea122994af2e762c683162ed02f07e0cead834
pulysse <ulysse.prevostlacaze@ynov.com>
2026-01-28 12:27:49 +0100

    peut etre

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// On définit la route racine "/" pour servir le fichier index.html
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	// On récupère le port (utile pour ton Procfile plus tard)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Port par défaut en local
+	}
+
+	fmt.Printf("Serveur lancé sur http://localhost:%s\n", port)
+
+	// Lancement du serveur
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		fmt.Println("Erreur lors du lancement :", err)
+	}
 }
